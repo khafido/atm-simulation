a12f77b9856b243697b1978d8448574c10705b1e
MITRAIS\Khafido_I343 <khafido.work@gmail.com>
2022-07-21 08:44:25 +0700

    remove previous stage-3

--- stage-3/src/main/java/com/mitrais/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mitrais.repository;
-
-import com.mitrais.entity.AccountEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AccountRepository extends JpaRepository<AccountEntity, String> {
-}
--- stage-3/src/main/java/com/mitrais/repository/WithdrawRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mitrais.repository;
-
-import com.mitrais.entity.WithdrawEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface WithdrawRepository extends JpaRepository<WithdrawEntity, UUID> {
-}
